dc2c863fb266abab9847cbb8c7a7254c871b9f98
Murad Aghayev <muradaghyv@gmail.com>
2024-12-18 17:44:51 +0400

    Modifying generate_response() method so that it directly generates a response without ResponseGenerator class. In the previous case, ResponseGenerator class used OllamaClient object for generating response. Now, ResponseGenerator class have been deleted.

--- src/llm/ollama_client.py
+++ src/llm/ollama_client.py
@@ -1,4 +1,7 @@
 import requests
+from typing import List
+from ..retrieval.search_result import SearchResult
+from ..llm.prompt_templates import PromptTemplates
 from ..utils.logging_utils import setup_logger
 
 logger = setup_logger(__name__)
@@ -8,10 +11,36 @@ class OllamaClient:
         self.model_name = model_name
         self.base_url = base_url
         self.api_endpoint = f"{self.base_url}/api/generate"
-    
-    def generate_response(self, prompt: str, system_prompt: str = None, temperature: float = 0.5) -> str:
-        """Generating response from Ollama."""
+
+    def generate_response(self, question: str, search_results: List[SearchResult],
+                          system_prompt: str = PromptTemplates.SYSTEM_PROMPT,
+                          temperature: float = 0.5, max_tokens: int = 500) -> str:
+        """
+        Generating a response with the help of Ollama Client. This response is generated according to 2 information:
+        input query and relevant documentation extracted by RAG mechanism. They are joined ang fed into the LLM client.
+
+        Args:
+            question: input query given by an user.
+            search_results: relevant information to the input query extracted via retrieval system.
+            system_prompt: Default prompt given to the LLM client 
+            temperature: controlling the randomness of the response. Lower value makes output less random
+                and more deterministic. Greater value makes the output more random, but more creative.
+            max_tokens: maximum token size of the response.
+        Returns:
+            LLM generated response.
+        """
         try:
+            # Combining text from the retrieval system results
+            context = "\n\n".join(
+                f"Source: {result.metadata.get('source', 'unknown')}\n{result.document}"
+                for result in search_results
+            )
+            
+            # Creating a prompt using template
+            prompt = PromptTemplates.create_prompt(context=context, 
+                                                   question=question)
+            
+            # Generate a response using LLM
             payload = {
                 "model": self.model_name,
                 "prompt": prompt,
@@ -19,19 +48,19 @@ class OllamaClient:
                 # Add parameters to control response
                 "temperature": temperature,
                 "top_p": 0.9,
-                "max_tokens": 500
+                "max_tokens": max_tokens,
+                "system": system_prompt
             }
-            if system_prompt: 
-                payload["system"] = system_prompt
 
             response = requests.post(self.api_endpoint, json=payload)
-            response.raise_for_status()
+            response.raise_for_status() # Status code (2xx is successfull). 4xx for client errors & 5xx for server errors
             
             return response.json().get("response", "")
             
         except requests.exceptions.RequestException as e:
-            logger.error(f"Error calling Ollama API: {str(e)}")
+            logger.error(f"Error generating LLM response: {str(e)}")
             raise
+        
 
 
             
